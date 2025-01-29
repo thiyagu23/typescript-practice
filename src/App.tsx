7ba4a72237895f0c1ba19ed245b113326519bcee
Thiyagu M <118154174+Thiyagaraj15@users.noreply.github.com>
2025-01-30 00:03:19 +0530

    datatypes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import DataTypes from './view/DataTypes'
 
 function App() {
+
+  //datatypes
+let name: string = "John";
+let age: number = 30;
+let isActive: boolean = true;
+let score: any = 100;
+  let unknownValue: unknown = "Hello";
+  
+  let ages: string = "20"
+  let names: string = "ssssss"
+  
+  console.log(names);
+
+  // duck typing
+  let data = { id: 1, name: "thiyagu" }
+  data = {name: "sss" , id: 1 }
+  
+  //array
+  let arrNumbers = {id:1,}
+  
+  console.log(arrNumbers);
+
+
+  
+  
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          TS Practice
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <DataTypes/> */}
       </header>
     </div>
   );
